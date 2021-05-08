8ffd38ec78c0d733b97109259a2beb1b4d838e20
Frank Jobst <fajobst@gmail.com>
2021-05-08 18:03:53 -0500

    customer-validation

--- src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/CustomerDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.util.UUID;
 
 /**
@@ -16,6 +17,10 @@ import java.util.UUID;
 @Builder
 public class CustomerDto {
 
+    @Null
     private UUID id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
     private String name;
 }
